0f832894496d6668b5d8e6eafb5fe0e0110e0fe8
Bryan Ng <ngkwokfai@gmail.com>
2020-08-04 03:35:38 +0800

    added score cards

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,34 @@ import './App.css';
 import GameClock from './GameClock';
 
 class App extends React.Component {
-  state = { timeLeft: null };
+  state = { timeLeft: null, awayScore: 0, homeScore: 0 };
 
+  awayPlusClick = () => {
+    this.setState({ awayScore: this.state.awayScore + 1 });
+  };
+
+  homePlusClick = () => {
+    this.setState({ homeScore: this.state.homeScore + 1 });
+  };
   render() {
     return (
       <div className="ui container mainContainer">
-        <div className="clock-container">
-          <GameClock
-            onNewTime={(newTime) => this.setState({ timeLeft: newTime })}
-          />
+        <div className="header">
+          <div className="away-score">{this.state.awayScore}</div>
+          <div className="clock-container">
+            <GameClock
+              onNewTime={(newTime) => this.setState({ timeLeft: newTime })}
+            />
+          </div>
+          <div className="home-score">{this.state.homeScore}</div>
+        </div>
+        <br />
+        <br />
+        <div style={{ display: 'inline-block', width: '100px' }}>
+          Away: <i className="large plus icon" onClick={this.awayPlusClick}></i>
+        </div>
+        <div style={{ display: 'inline-block', width: '100px' }}>
+          Home: <i className="large plus icon" onClick={this.homePlusClick}></i>
         </div>
       </div>
     );
